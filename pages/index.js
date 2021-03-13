2b9051049cd4970c31009e539576ed195deb6b1d
Vincent Lim Show Chen <bereninga@gmail.com>
2021-03-13 04:03:09 -0500

    Button

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: black;
+`
 
 export default function Home() {
   return (
@@ -8,6 +13,8 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Button>Yes</Button>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
